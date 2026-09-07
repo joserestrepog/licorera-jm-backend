aa0ed72a6961aedeed1cead6b7a5cebb6f9d9716
Jose Luis Restrepo Gesamá <jose1.restrepo@ucp.edu.co>
2026-09-07 14:31:39 -0500

    feat: initial project setup

---
+++ src/main/java/com/licorerajm/backend/LicoreraJmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.licorerajm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicoreraJmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicoreraJmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/licorerajm/backend/LicoreraJmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licorerajm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicoreraJmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
